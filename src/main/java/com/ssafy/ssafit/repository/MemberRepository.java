ec1c92154ba4ac0147c5864cf9270d97b810cd45
Joslynn20 <joslynnkim@gmail.com>
2023-05-18 16:33:51 +0900

    [+] 보경 MemberService 로그인 기능 수정 230518

--- src/main/java/com/ssafy/ssafit/repository/MemberRepository.java
+++ src/main/java/com/ssafy/ssafit/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package com.ssafy.ssafit.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ssafy.ssafit.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
-	Member findByMemberIdAndPassword(Member member);
+	Member findByMemberIdAndPassword(@Param("memberId") String memberId, @Param("password") String password);
 }
