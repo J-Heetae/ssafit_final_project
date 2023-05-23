fc94164ce3ea2eb94d96cb34e1ba9f9625380f39
Jeong TaeHee <th1563@naver.com>
2023-05-23 21:47:36 +0900

    [+] 태희 Mod 영상 서비스 구현 및 수정 230523

--- src/main/java/com/ssafy/ssafit/controller/VideoController.java
+++ src/main/java/com/ssafy/ssafit/controller/VideoController.java
@@ -1,28 +1,108 @@
 package com.ssafy.ssafit.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ssafit.domain.Video;
+import com.ssafy.ssafit.dto.VideoDto;
 import com.ssafy.ssafit.service.VideoService;
-import com.ssafy.ssafit.service.VideoServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/video")
+@RequestMapping("/api/video")
 public class VideoController {
 	
-	private final VideoServiceImpl videoService;
+	private final VideoService videoService;
 	
+	/**
+	 * 해당 번호와 일치하는 영상 검색
+	 * 
+	 * @param no 영상 번호
+	 */
 	@GetMapping("/{no}")
-	public ResponseEntity<Video> findVideo(@PathVariable("no") Long no)  {
-		return new ResponseEntity<Video>(videoService.findByNo(no), HttpStatus.OK);
+	public ResponseEntity<VideoDto> findVideo(@PathVariable("no") Long no)  {
+		return new ResponseEntity<VideoDto>(videoService.findByNo(no), HttpStatus.OK);
+	}
+	
+	/**
+	 * 특정컬럼에서 검색어에 해당하는 영상 조회
+	 */
+	@GetMapping("/search")
+    public ResponseEntity<List<VideoDto>> searchVideos(
+            @RequestParam String keyword,
+            @RequestParam String option) {
+        List<VideoDto> videos = videoService.searchVideos(keyword, option);
+        return ResponseEntity.ok(videos);
+    }
+	
+	/**
+	 * 조건에 맞춰 전체 영상 조회
+	 * 
+	 * @param partName 운동 부위
+	 * @param orderDirection 정렬방향
+	 * @param orderCondition 정렬조건
+	 * @param pageable
+	 */
+	@GetMapping
+	public ResponseEntity<List<VideoDto>> findAllVideos(
+			@RequestParam(required = false) String partName,
+			@RequestParam(defaultValue = "DESC") String orderDirection,
+			@RequestParam(defaultValue = "regDate") String orderCondition) {
+		return new ResponseEntity<List<VideoDto>>(
+				videoService.findAll(partName, orderCondition, orderDirection), HttpStatus.OK);
+	}
+	
+	/**
+	 * 영상 등록
+	 * 
+	 * @param video 등록할 영상
+	 * @return 등록된 영상
+	 */
+	@PostMapping
+	public ResponseEntity<Video> registVideo(Video video) {
+		return new ResponseEntity<Video>(videoService.insert(video), HttpStatus.CREATED);
 	}
 
+	/**
+	 * 영상 수정
+	 * 
+	 * @param updateVideo 수정할 영상
+	 * @return 수정된 영상
+	 */
+	@PutMapping
+	public ResponseEntity<Video> updateVideo(Video updateVideo) {
+		return new ResponseEntity<Video>(videoService.update(updateVideo), HttpStatus.OK);
+	}
+	
+	/**
+	 * 영상 삭제
+	 * 
+	 * @param no 삭제할 영상 번호
+	 * @return
+	 */
+	@DeleteMapping("/{no}")
+	public ResponseEntity<Void> deleteVideo(@PathVariable("no") Long no) {
+		videoService.delete(no);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+//	/**
+//	 * 해당 영상의 모든 댓글 조회하기
+//	 */
+//	@GetMapping("/comment")
+//	public ResponseEntity<List<VideoCommentDto>> findComments(@RequestParam("no") Long videoNo) {
+//		return new ResponseEntity<List<VideoCommentDto>>(videoCommentService.findAllComments(videoNo), HttpStatus.OK);
+//	}
 }
--- src/main/java/com/ssafy/ssafit/service/VideoService.java
+++ src/main/java/com/ssafy/ssafit/service/VideoService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import com.ssafy.ssafit.domain.Video;
+import com.ssafy.ssafit.dto.VideoDto;
 import com.ssafy.ssafit.exception.DuplicatedException;
 import com.ssafy.ssafit.exception.NotFoundException;
 
@@ -22,14 +24,11 @@ public interface VideoService {
 	Video update(Video video) throws DuplicatedException;
 	
 	//영상 번호로 영상 조회
-	Video findByNo(Long no) throws NotFoundException;
+	VideoDto findByNo(Long no) throws NotFoundException;
+	
+	//검색어랑 검색 기준으로 영상 조회
+	List<VideoDto> searchVideos(String keyword, String searchCriteria);
 	
 	//모든 영상 조회
-	Page<Video> findAll(Pageable pageable, String partName, String orderCondition, String orderDirection);
-	
-	//운동 부위로 영상 조회
-	List<Video> findByPartName(String partName);
-	
-	//정렬 기준으로 영상 조회
-	List<Video> findOrderByViewCnt(String orderDir);
+	List<VideoDto> findAll(String partName, String orderCondition, String orderDirection);
 }
